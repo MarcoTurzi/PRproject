6d9078760d8a8ab29344829b3f0ad5bfe941428c
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2023-01-21 14:26:30 +0100

    Update resnet50training.py

--- resnet50training.py
+++ resnet50training.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from keras import layers
 from sklearn.metrics import classification_report, confusion_matrix
-
+import cv2
 
 
 # Define the path to the train and test sets
@@ -19,7 +19,7 @@ train_ds, val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     train_path,
     validation_split=0.2,
     image_size=(299,299),
-    batch_size=32,
+    batch_size=16,
     seed=123,
     subset="both")
 
@@ -30,26 +30,37 @@ train_ds, val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 test_ds_cauc = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_cauc,
     image_size=(299,299),
-    batch_size=32,
+    batch_size=1,
     seed=123)
 
 test_ds_afro = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_afro,
     image_size=(299,299),
-    batch_size=32,
+    batch_size=1,
     seed=123)
 
 test_ds_asian = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_asian,
     image_size=(299,299),
-    batch_size=32,
+    batch_size=1,
     seed=123)
 
 test_ds_indian = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_indian,
     image_size=(299,299),
-    batch_size=32,
+    batch_size=1,
     seed=123)
+
+labels = np.array([])
+images = np.array([])
+pred = np.array([])
+k = 0
+for i,l in test_ds_cauc.take(-1):
+    labels = np.append(labels,l)
+    if k == 0:
+        imag = np.expand_dims(i.numpy(),axis=0)
+        print(imag.shape)
+    k = k + 1
 
 model = tf.keras.Sequential()
 
@@ -78,30 +89,55 @@ model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 # Compile the model
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
+              metrics=['accuracy',
+               'binary_accuracy',
+                'categorical_accuracy',
+                 'sparse_categorical_accuracy',
+                  'top_k_categorical_accuracy',
+                   'sparse_top_k_categorical_accuracy',
+                    'mean_squared_error',
+                     'mean_absolute_error',
+                      'mean_absolute_percentage_error',
+                       'cosine_proximity'])
 
 early_stopping = tf.keras.callbacks.EarlyStopping(
-    min_delta=0.01, # minimium amount of change to count as an improvement
-    patience=2, # how many epochs to wait before stopping
+    min_delta=0.0001, # minimium amount of change to count as an improvement
+    patience=3, # how many epochs to wait before stopping
     restore_best_weights=True,
 )
 
 # Train the model
 history = model.fit(train_ds,
     validation_data = val_ds,
-    epochs=10,
+    epochs=15,
+    verbose=0,
     callbacks = [early_stopping])
 
+print("CAUCASIAN")
+print(model.evaluate(x = test_ds_cauc,
+            verbose=0,
+            return_dict=True))
+print("AFRO-AMERICAN")
+print(model.evaluate(x = test_ds_afro,
+            verbose=0,
+            return_dict=True))
+print("ASIAN")
+print(model.evaluate(x = test_ds_asian,
+            verbose=0,
+            return_dict=True))
+print("INDIAN")
+print(model.evaluate(x = test_ds_indian,
+            verbose=0,
+            return_dict=True))
 
-
-labels = np.array([])
+'''labels = np.array([])
 images = np.array([])
+pred = []
 for i,l in test_ds_cauc.take(-1):
     labels = np.append(labels,l)
-    images = np.append(images, i)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
 
-pred = model.predict(images)
-pred = pred.reshape(-1,1)
 
 print("CAUCASIAN")
 print(classification_report(labels, pred))
@@ -109,12 +145,11 @@ print(confusion_matrix(labels, pred))
 
 labels = np.array([])
 images = np.array([])
+pred = []
 for i,l in test_ds_afro.take(-1):
     labels = np.append(labels,l)
-    images = np.append(images, i)
-
-pred = model.predict(images)
-pred = pred.reshape(-1,1)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
 
 print("AFRO-AMERICAN")
 print(classification_report(labels, pred))
@@ -122,12 +157,12 @@ print(confusion_matrix(labels, pred))
 
 labels = np.array([])
 images = np.array([])
+pred = []
 for i,l in test_ds_indian.take(-1):
     labels = np.append(labels,l)
-    images = np.append(images, i)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
 
-pred = model.predict(images)
-pred = pred.reshape(-1,1)
 
 print("INDIAN")
 print(classification_report(labels, pred))
@@ -135,17 +170,17 @@ print(confusion_matrix(labels, pred))
 
 labels = np.array([])
 images = np.array([])
+pred = []
 for i,l in test_ds_asian.take(-1):
     labels = np.append(labels,l)
-    images = np.append(images, i)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
 
-pred = model.predict(images)
-pred = pred.reshape(-1,1)
 
 print("ASIAN")
 print(classification_report(labels, pred))
 print(confusion_matrix(labels, pred))
-
+'''
 history_df = pd.DataFrame(history.history)
 history_df.loc[0:, ['accuracy', 'val_accuracy']].plot()
 

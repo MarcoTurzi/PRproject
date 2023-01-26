140765e61e4bc264d47a3569efe289349ff8c046
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2023-01-26 15:59:11 +0100

    Update resnet50training.py

--- resnet50training.py
+++ resnet50training.py
@@ -5,14 +5,16 @@ import pandas as pd
 from keras import layers
 from sklearn.metrics import classification_report, confusion_matrix
 import cv2
+import os
 
 
 # Define the path to the train and test sets
-train_path = 'final_train_dataset'
+train_path = 'mixed10000_class_train_set'
 test_path_afro = 'new_data_test_set\\afro-american'
 test_path_cauc = 'new_data_test_set\\caucasian'
 test_path_asian = 'new_data_test_set\\asian'
 test_path_indian = 'new_data_test_set\\indian'
+
 
 # Create the train dataset
 train_ds, val_ds = tf.keras.preprocessing.image_dataset_from_directory(
@@ -25,13 +27,16 @@ train_ds, val_ds = tf.keras.preprocessing.image_dataset_from_directory(
 
 
 
-
 # Create the test dataset
 test_ds_cauc = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_cauc,
     image_size=(299,299),
     batch_size=1,
     seed=123)
+images= []
+print(test_ds_cauc)
+
+
 
 test_ds_afro = tf.keras.preprocessing.image_dataset_from_directory(
     test_path_afro,
@@ -50,8 +55,6 @@ test_ds_indian = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(299,299),
     batch_size=1,
     seed=123)
-
-
 
 model = tf.keras.Sequential()
 
@@ -80,20 +83,11 @@ model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 # Compile the model
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
-              metrics=['accuracy',
-               'binary_accuracy',
-                'categorical_accuracy',
-                 'sparse_categorical_accuracy',
-                  'top_k_categorical_accuracy',
-                   'sparse_top_k_categorical_accuracy',
-                    'mean_squared_error',
-                     'mean_absolute_error',
-                      'mean_absolute_percentage_error',
-                       'cosine_proximity'])
+              metrics=['accuracy'])
 
 early_stopping = tf.keras.callbacks.EarlyStopping(
     min_delta=0.0001, # minimium amount of change to count as an improvement
-    patience=3, # how many epochs to wait before stopping
+    patience=5, # how many epochs to wait before stopping
     restore_best_weights=True,
 )
 
@@ -101,14 +95,69 @@ early_stopping = tf.keras.callbacks.EarlyStopping(
 history = model.fit(train_ds,
     validation_data = val_ds,
     epochs=15,
-    verbose=0,
     callbacks = [early_stopping])
 
+model.save('model\\modelbinary15')
+
+acc_test,len = 0., 0.
+y_pred = np.array([])
+y_true = np.array([])
+for i,(x,y) in enumerate(test_ds_cauc):
+  '''y_pred= np.append(y_pred,tf.argmax(model.predict(x),axis=1))
+  y_true= np.append(y_true,y.numpy()[0])'''
+  y_pred = np.append(y_pred,1 if model.predict(x, verbose=0)[0][0] > 0.5 else 0)
+  y_true = np.append(y_true, y.numpy()[0])
+  
+
+
+print(y_pred)
+print(y_true)
+
 print("CAUCASIAN")
-print(model.evaluate(x = test_ds_cauc,
-            verbose=0,
-            return_dict=True))
+print(classification_report(y_true,y_pred))
+print(confusion_matrix(y_true,y_pred))
+
+y_pred = np.array([])
+y_true = np.array([])
+for i,(x,y) in enumerate(test_ds_afro):
+  '''y_pred= np.append(y_pred,tf.argmax(model.predict(x),axis=1))
+  y_true= np.append(y_true,y.numpy()[0])'''
+  y_pred = np.append(y_pred,1 if model.predict(x, verbose=0)[0][0] > 0.5 else 0)
+  y_true = np.append(y_true, y.numpy()[0])
+  
+
+
 print("AFRO-AMERICAN")
+print(classification_report(y_true,y_pred))
+print(confusion_matrix(y_true,y_pred))
+
+y_pred = np.array([])
+y_true = np.array([])
+for i,(x,y) in enumerate(test_ds_indian):
+  '''y_pred= np.append(y_pred,tf.argmax(model.predict(x),axis=1))
+  y_true= np.append(y_true,y.numpy()[0])'''
+  y_pred = np.append(y_pred,1 if model.predict(x, verbose=0)[0][0] > 0.5 else 0)
+  y_true = np.append(y_true, y.numpy()[0])
+  
+
+
+print("INDIAN")
+print(classification_report(y_true,y_pred))
+print(confusion_matrix(y_true,y_pred))
+
+y_pred = np.array([])
+y_true = np.array([])
+for i,(x,y) in enumerate(test_ds_asian):
+  '''y_pred= np.append(y_pred,tf.argmax(model.predict(x),axis=1))
+  y_true= np.append(y_true,y.numpy()[0])'''
+  y_pred = np.append(y_pred,1 if model.predict(x, verbose=0)[0][0] > 0.5 else 0)
+  y_true = np.append(y_true, y.numpy()[0])
+  
+
+print("ASIAN")
+print(classification_report(y_true,y_pred))
+print(confusion_matrix(y_true,y_pred))
+'''print("AFRO-AMERICAN")
 print(model.evaluate(x = test_ds_afro,
             verbose=0,
             return_dict=True))
@@ -120,8 +169,58 @@ print("INDIAN")
 print(model.evaluate(x = test_ds_indian,
             verbose=0,
             return_dict=True))
+'''
+'''labels = np.array([])
+images = np.array([])
+pred = []
+for i,l in test_ds_cauc.take(-1):
+    labels = np.append(labels,l)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
 
 
+print("CAUCASIAN")
+print(classification_report(labels, pred))
+print(confusion_matrix(labels, pred))
+
+labels = np.array([])
+images = np.array([])
+pred = []
+for i,l in test_ds_afro.take(-1):
+    labels = np.append(labels,l)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
+
+print("AFRO-AMERICAN")
+print(classification_report(labels, pred))
+print(confusion_matrix(labels, pred))
+
+labels = np.array([])
+images = np.array([])
+pred = []
+for i,l in test_ds_indian.take(-1):
+    labels = np.append(labels,l)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
+
+
+print("INDIAN")
+print(classification_report(labels, pred))
+print(confusion_matrix(labels, pred))
+
+labels = np.array([])
+images = np.array([])
+pred = []
+for i,l in test_ds_asian.take(-1):
+    labels = np.append(labels,l)
+    imag = np.expand_dims(i.numpy(),axis=0)
+    pred.append(1 if model.predict(imag.reshape((1,299,299,3)), verbose=0)[0][0] > 0.5 else 0)
+
+
+print("ASIAN")
+print(classification_report(labels, pred))
+print(confusion_matrix(labels, pred))
+'''
 history_df = pd.DataFrame(history.history)
 history_df.loc[0:, ['accuracy', 'val_accuracy']].plot()
 
